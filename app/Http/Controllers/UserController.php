ecaab086a6a4661672cb6cb30c0fdb74a6c7f2b8
Reenjay <reenjie17@gmail.com>
2023-02-09 16:27:26 +0800

    2/9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class UserController extends Controller
 {
     public function signIn(Request $request)
     {
+        $credentials = array(
+            'username' => $request->username,
+            'password' => $request->password
+        );
 
-        dd($request);
+        if (Auth::attempt($credentials)) {
+            echo 'success';
+        } else {
+            echo 'failed';
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::namespace('App\Http\Controllers')->group(function () {
-    Route::post('signin', 'UserController@signIn');   //Login
+    Route::post('login', 'UserController@signIn');   //Login
 
 
 });
