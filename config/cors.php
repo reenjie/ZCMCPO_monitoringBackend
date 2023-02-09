a47fec391a073698444663e0a5239e9795a79f2e
Reenjay <reenjie17@gmail.com>
2023-02-09 10:39:06 +0800

    initial commit

---
+++ config/cors.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'allow_credentials' => true,
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*',],
+
+    'allowed_methods' => ['PUT,DELETE,POST,GET,OPTIONS', '*'],
+
+    'allowed_origins' => ['http://localhost:3000', "htt://192.168.13.232:3000", '*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ["Accept, Authorization, Content-Type", '*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+
+];
